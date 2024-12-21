5a1b7bbead59c02fe257460abad7879e9005e0b2
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-12-22 04:40:43 +0900

    refacto: Text messages handling started

--- src/main.py
+++ src/main.py
@@ -7,20 +7,18 @@ from src.config import config
 async def main():
     config.LOGGER.info("Starting bot...")
     try:
-        # TODO: check if this is still needed
-        # Get the bot's username and set the persona name
-        # bot_info = await BOT.get_me()
-        # LOGGER.info(f"Bot started: {bot_info.username}")
-        # set_bot_name(bot_info.username)
+        # Get the bot's username
+        bot_info = await config.BOT.get_me()
+        config.BOT_INFO = bot_info
+        config.LOGGER.info(f"Bot started: {bot_info.username}")
+
         await register_commands()
         config.LOGGER.info("Commands registered")
         await config.BOT.polling()
     except Exception as e:
         config.LOGGER.error(f"An unexpected error occurred: {e}")
     finally:
-        config.LOGGER.info("Stopping Bot...")
-        # Close all open connections
-        # await agent.clear_all_chats()
+        config.LOGGER.info("Stopping bot...")
 
 
 if __name__ == "__main__":
